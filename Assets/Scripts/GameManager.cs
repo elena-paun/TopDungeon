bd8d77319559c70e0ca018dd6a4ea761008f9b52
Elena Paun <elena.paun@thinslices.com>
2022-04-20 10:42:44 +0300

    TopDungeon first level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Debug.Log("awake");
+        PlayerPrefs.DeleteAll();
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    // References
+    public Player player;
+
+    // Logic
+    public int pesos;
+    public int experience;
+
+    // Save state
+    /*
+     * INT preferredSkin
+     * INT pesos
+     * INT experience
+     * INT weaponLevel
+     */
+    public void SaveState()
+    {
+        string s = "";
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+        // PlayerPrefs.SetString("SaveState", s);
+        Debug.Log("Save state");
+    }
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        if(!PlayerPrefs.HasKey("SaveState"))
+            return;
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+        // 0|10|15
+        Debug.Log(data);
+        // Change player skin
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        // Change weapon level
+        Debug.Log("Load state");
+    }
+}
