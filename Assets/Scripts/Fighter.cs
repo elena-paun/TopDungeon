e8d417b10f32007b121932290876cb53e1d579ae
Elena Paun <elena.paun@thinslices.com>
2022-04-27 10:53:59 +0300

    defining enemy scripts

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Fighter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // public fields
+    public int hitpoint = 10;
+    public int maxHitpoint = 10;
+    public float pushRecoverySpeed = 0.2f;
+
+    // Immunity
+    protected float immuneTime = 1.0f;
+    protected float lastImmune;
+
+    // Push
+    protected Vector3 pushDirection;
+
+    // All fighters can ReceiveDamage/ die
+    protected virtual void ReceiveDamage(Damage dmg)
     {
-        
+        if (Time.time - lastImmune > immuneTime)
+        {
+            lastImmune = Time.time;
+            hitpoint -= dmg.damageAmount;
+            pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
+
+            GameManager.instance.ShowText(dmg.damageAmount.ToString(), 25, Color.red, transform.position, Vector3.zero, 0.5f);
+
+            if (hitpoint <= 0)
+            {
+                hitpoint = 0;
+                Death();
+            }
+        }
+    }
+    protected virtual void Death()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
